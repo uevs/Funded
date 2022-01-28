4b7b67e12f0ee468ffd90243d501210f5e5b7137
uevs <36189306+uevs@users.noreply.github.com>
2022-01-28 17:55:36 +0100

    Initial Commit

---
+++ Funded/FundedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundedApp.swift
+//  Funded
+//
+//  Created by leonardo on 28/01/22.
+//
+
+import SwiftUI
+
+@main
+struct FundedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
