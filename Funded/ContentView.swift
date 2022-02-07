3c5976401ee02d1cd098a0ab4f1aa154fba45052
aristidelauga <aristide.lauga@gmail.com>
2022-02-07 17:45:39 +0100

    SearchBar Implemented

--- Funded/ContentView.swift
+++ Funded/ContentView.swift
@@ -12,18 +12,39 @@ struct ContentView: View {
     //Grants are stored here
     var allGrants = AvailableGrants()
     
+    @State private var searchText: String = ""
+    
+    private var searchResults: [Grant] {
+        let searchResults = allGrants.grants
+        if searchText.isEmpty { return searchResults }
+        return searchResults.filter {
+            $0.name.lowercased().contains( searchText.lowercased()) || $0.categories.contains(searchText)
+        }
+    }
+    
+    
+    
+    //private var filterResults: [Grant] {
+        let filterResults = allGrants.grants
+       // if
+    //}
+    
     
     var body: some View {
         NavigationView {
             ScrollView {
-                VStack {
-                    ForEach(allGrants.grants, id: \.self) { grant in
+                VStack(spacing: 12) {
+                    ForEach(searchResults) { grant in
                         CardView(grant: grant)
-                            .padding(.bottom, 10)
                     }
                 }
             }
             .navigationTitle("Funded")
+            .searchable(
+                text: $searchText,
+                placement: .navigationBarDrawer(displayMode: .always),
+                prompt: "Search for grant"
+            )
             .toolbar(content: {Image(systemName: "star.fill")})
         }
     }
